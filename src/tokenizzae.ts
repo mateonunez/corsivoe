bb60caeab854bc71223efd51f34437344351ebcc
mateonunez <mateonunez95@gmail.com>
2022-07-26 18:46:04 +0200

    feat(tokenizzae): add methods and npl package

---
+++ src/tokenizzae.ts
@@ -0,0 +1,17 @@
+const { NormalizerIt, TokenizerIt } = require('@nlpjs/lang-it');
+
+export function normalize(text: string): string {
+  const normalizer = new NormalizerIt();
+
+  return normalizer.normalize(text);
+}
+
+export function tokenize(text: string): string[] {
+  const tokenizer = new TokenizerIt();
+
+  return tokenizer.tokenize(text);
+}
+
+export default function tokenizzae(text: string): string[] {
+  return tokenize(normalize(text));
+}
